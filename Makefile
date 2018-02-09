07eda26f398d97d3f1d8fc3c33bf2effede5d18b
Hoshoyo <pveiga1991@hotmail.com>
2018-02-09 00:30:21 -0200

    project cleanup

--- Makefile
+++
@@ -1,2 +0,0 @@
-all:
-	g++ -g -O2 -std=c++11 main.cpp chess.cpp ui.cpp util.cpp minmax.cpp -o hochess
--- util.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "common.h"
-
-s8* read_console(u32* out_length);
-bool string_equal(const s8* s1, const s8* s2);
-bool string_equal_until_length(const s8* s1, const s8* s2, u32 length);
-void eat_spaces(s8** at);
